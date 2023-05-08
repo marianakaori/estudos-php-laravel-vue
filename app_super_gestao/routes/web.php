9a70922b7cab3f5bc07804fe377b9346c571eaa1
Mariana Kaori <mariana.kaori@usp.br>
2023-05-08 11:36:51 -0300

    redirecionamento de rotas e rota de fallback

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -23,3 +23,22 @@ Route::prefix('/app')->group(function () {
     Route::get('/fornecedores', function () { return 'Fornecedores'; })->name('app.fornecedores');
     Route::get('/produtos', function () { return 'Produtos'; })->name('app.produtos');
 });
+
+// Redirecionamento dentro da função de callback (pode ser usado no controller)
+Route::get('/rota1', function () {
+    echo 'rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function () {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+// Redirecionamento direto nas rotas
+// Route::redirect('rota2', 'rota1');
+
+Route::fallback(function () {
+    echo
+        'A rota acessada não existe.
+        <a href="'.route('site.index').'"> Clique aqui</a>
+        para voltar para a página inicial.';
+});
