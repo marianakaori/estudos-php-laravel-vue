b6dfaa9dd93844dd074fa9d8a48d7770cc6dac61
Mariana Kaori <mariana.kaori@usp.br>
2023-05-15 23:43:49 -0300

    validação dos campos do formulário e repopulação old input

--- app_super_gestao/resources/views/site/contato.blade.php
+++ app_super_gestao/resources/views/site/contato.blade.php
@@ -10,7 +10,7 @@
         
         <div class="informacao-pagina">
             <div class="contato-principal">
-                @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+                @component('site.layouts._components.form_contato', ['classe' => 'borda-preta', 'motivo_contatos' => $motivo_contatos])
                     <p>A nossa equipe analisará sua mensagem e retornará o mais breve possível!</p>
                     <p>Nosso tempo médio de respota é de 48 horas.</p>
                 @endcomponent
