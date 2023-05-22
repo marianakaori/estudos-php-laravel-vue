39514bb3eca40f8d2bd90573dc7a294e2490c8b9
Mariana Kaori <mariana.kaori@usp.br>
2023-05-22 20:49:39 -0300

    customização da apresentação das mensagens de erro na validação do formulário

--- app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
+++ app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,10 +2,15 @@
 <form action="{{ route('site.contato') }}" method="POST">
     @csrf
     <input name="nome" value="{{old('nome')}}" type="text" placeholder="Nome" class="{{ $classe }}">
+    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
+
     <input name="telefone" value="{{old('telefone')}}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
     <br>
+    
     <input name="email" value="{{old('email')}}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    {{ $errors->has('email') ? $errors->first('email') : '' }}
     <br>
 
     <select name="motivo_contatos_id" class="{{ $classe }}">
@@ -15,14 +20,21 @@
             <option value="{{$motivo_contato->id}}" {{ old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : '' }}>{{$motivo_contato->motivo_contato}}</option>
         @endforeach
     </select>
+    {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}
     <br>
+
     <textarea name="mensagem" class="{{ $classe }}">{{ old('mensagem') != '' ? old('mensagem') : 'Preencha aqui a sua mensagem' }}</textarea>
+    {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
+
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
-<div style="position:absolute; top:0px; left:0px; width:100%; background-color:red">
-    <pre>
-    {{ print_r($errors) }}
-    </pre>
-</div>
+@if ($errors->any())
+    <div style="position:absolute; top:0px; left:0px; width:100%; background-color:red">
+        @foreach ($errors->all() as $error)
+            {{$error}}
+            <br />
+        @endforeach
+    </div>
+@endif
