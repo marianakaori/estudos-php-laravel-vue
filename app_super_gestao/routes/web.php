c159172c28053ef8d1860e2509713c4028417a27
Mariana Kaori <mariana.kaori@usp.br>
2023-05-22 21:57:15 -0300

    middleware LogAcesso

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::middleware(LogAcessoMiddleware::class)
+    ->get('/', 'PrincipalController@principal')
+    ->name('site.index')
+;
+
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
-Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::middleware(LogAcessoMiddleware::class)
+    ->get('/contato', 'ContatoController@contato')
+    ->name('site.contato')
+;
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 Route::get('/login', function () { return 'Login'; })->name('site.login');
 
